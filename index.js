0b54b16da754451f7ca469049c725cb3a0c0bee1
arman chakawala <armanchakawala@armans-MacBook-Air.local>
2024-11-05 13:57:12 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Example 1
+
+//const num1 =5;
+//const num2 =15;
+
+//let sum = num1 + num2;
+
+//console.log("The sum of" , num1 , "and" , num2 , "is" ,sum); 
+
+//by putting "+" insted of "," it will not make space in output.
+//console.log("the sum of" + num1 + "and"  + num2 +"is",sum);
+
+//example 2 not work even in class
+
+//finall portfolio
+
+const num1 = 5;
+const num2 = 10;
+const num3 =15;
+const num4=20;
+
+const test = num1+(num2-num3)*num4/num1;
+
+//console.log(test);
+
+console.log("the sum of" , num1 , "by subtracting" , num2 , "from" , num3 , "then multiplying with" , num4 , "and divide by" , num1 , "is",test);
